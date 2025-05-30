d33f0f6dc6e73afd23d4291dac4eab64cfc75036
Onion Madder <onionmadder@gmail.com>
2025-05-29 21:49:07 -0700

    Create poke.js

---
+++ js/char/wormy/poke.js
@@ -0,0 +1,12 @@
+export const pokeResponses = [
+  "blinked slowly. unsure if it was a game.",
+  "twitched once. maybe on purpose.",
+  "rolled away. but not far.",
+  "stared back. unblinking. wriggling.",
+  "pressed his face to the glass. daring you.",
+  "pulsed hard against the terminal wall. eager?",
+  "shivered. soft static echoed.",
+  "opened a little. not wide. just enough.",
+  "glowed once, then dimmed. like a warning.",
+  "stopped moving. but didn’t stop watching."
+];
