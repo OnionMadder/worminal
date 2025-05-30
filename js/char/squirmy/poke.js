d0b438ee9032266f425e6c3829c74c670161675b
Onion Madder <onionmadder@gmail.com>
2025-05-29 21:47:55 -0700

    Create poke.js

---
+++ js/char/squirmy/poke.js
@@ -0,0 +1,12 @@
+export const pokeResponses = [
+  "flinched a little. shy.",
+  "squirmed in a tight spiral.",
+  "peeked at you. then looked away.",
+  "clicked softly. nervous rhythm.",
+  "twitched like he thought it was punishment.",
+  "started glowing. then stopped. shy joy?",
+  "wrote your name in dots. then erased it.",
+  "pushed back. just a little.",
+  "made a low humming sound. it vibrated the glass.",
+  "froze completely. pretending you weren’t there."
+];
